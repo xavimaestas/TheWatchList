5f439c0e8fdbdb8e7c777d27329b6f3c634d4d63
xavimaestas <xavimaestas@gmail.com>
2022-12-15 16:47:25 -0700

    implemented search and loop of movies
    added for loop and put html into JS

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 const search = document.getElementById("search-btn")
 const title = document.getElementById("title")
 const rating = document.getElementById("rating")
-const runtime = document.getElementById("runtime")
+const year = document.getElementById("year")
 const genre = document.getElementById("genre")
 const description = document.getElementById("movie-description")
 const movieImg = document.getElementById("movie-img")
 const watchLink = document.getElementById("watch-movie")
-const tomatoImg ="tomato.png"
+
 
 
     search.addEventListener("click", () => {
         const input = document.getElementById("search-input").value
-        fetch(`https://www.omdbapi.com/?t=${input}&apikey=88e64e6b&s`)
+        fetch(`https://www.omdbapi.com/?s=${input}&apikey=88e64e6b&s`)
         .then(result => result.json())
         .then(data => {
             console.log(data)
-            console.log(data.imdbID)
+            console.log(data.Search[0].Title)
             console.log(data.Title)
-            console.log(data.Ratings[1].Value)
-            title.textContent = data.Title
-            rating.innerHTML = `
-            <img id="tomatoImg" src=${tomatoImg}>${data.Ratings[1].Value}`
-            runtime.textContent = data.Runtime
-            genre.textContent = data.Genre
-            description.textContent = data.Plot
-            movieImg.src = data.Poster
-            watchLink.innerHTML = `
-            <a href="https://vidsrc.me/embed/${data.imdbID}/" target="_"><p>Watch Movie</p></a>`
+            // movie block HTML
 
-           
+            for(let movie = 0; movie < 5; movie ++){
+            document.getElementById("movie-block").innerHTML += `
+            <div id="text-block">
+            <div id="title-rating-div">
+                <h2 id="title">${data.Search[movie].Title}</h2>
+                <p id="year">${data.Search[movie].Year}</p>
+            </div>
+
+            <div id="genre-runtime-div">
+                
+                <p id="genre">${data.Search[movie].Genre}</p>
+                <button id="watchlist-btn">add to watchlist</button>
+                <div id="watch-movie"><a href="https://vidsrc.me/embed/${data.Search[movie].imdbID}/" target="_"><p>Watch Movie</p></a></div>
+            </div>
+
+            <div id="description-div">
+                <p id="movie-description">${data.Search[movie].Plot}</p>
+            </div>
+        </div>
+
+        <img src="${data.Search[movie].Poster}" id="movie-img">
+        `
+            }   
+// insert other fetch here that grabs other info that fetch can't 
+        
         })
-        console.log("what")
+        
     })
 
 
