a60f17cd244a60e4ea4f81fed303090725f6e006
xavimaestas <xavimaestas@gmail.com>
2022-12-15 22:39:27 -0700

    changed to await promise.all
    used promise.all with both fetch links, but think I need to switch to async await functions. I need to get the data first from the search and then input data from that search into the single movie search fetch..

--- index.js
+++ index.js
@@ -6,46 +6,55 @@ const genre = document.getElementById("genre")
 const description = document.getElementById("movie-description")
 const movieImg = document.getElementById("movie-img")
 const watchLink = document.getElementById("watch-movie")
+let movieArray = []
+
+
+    // Promise.all([
+    //     fetch("https://www.omdbapi.com/?s=${input}&apikey=88e64e6b&s").then(res => res.json),
+    //     fetch("https://www.omdbapi.com/?t=${input}&apikey=88e64e6b&s").then(res => res.json)
+    //     // data.Search[movie].Title
+    // ]).then(movies => console.log(movies))
+
+    
+    
+        
 
 
 
     search.addEventListener("click", () => {
         const input = document.getElementById("search-input").value
-        fetch(`https://www.omdbapi.com/?s=${input}&apikey=88e64e6b&s`)
-        .then(result => result.json())
-        .then(data => {
-            console.log(data)
-            console.log(data.Search[0].Title)
-            console.log(data.Title)
-            // movie block HTML
+        let urls = [
+            `https://www.omdbapi.com/?s=${input}&apikey=88e64e6b&s`,
+            `https://www.omdbapi.com/?t=${input}&apikey=88e64e6b&s`
+        ]
+            const fetchData = async () => {
+                try {
+                const response = await Promise.all(
+                    urls.map(url => fetch(url).then(res => res.json()))
+                ).then(data => {
+                    console.log(data)
+                    for(let movie = 0; movie < 5; movie ++){
+                        let imd = data[0].Search[movie].imdbID
+                        document.getElementById("movie-block").innerHTML += `
+                                        <div id="text-block">
+                                        <div id="title-rating-div">
+                                            <h2 id="title">${data[0].Search[movie].Title}</h2>
+                                            <p id="year">${data[1].Actors}</p>
+                                        </div>`
+                                        movieArray.push(data[0].Search[movie].Title)
+                                        console.log(movieArray)
+                    }
 
-            for(let movie = 0; movie < 5; movie ++){
-            document.getElementById("movie-block").innerHTML += `
-            <div id="text-block">
-            <div id="title-rating-div">
-                <h2 id="title">${data.Search[movie].Title}</h2>
-                <p id="year">${data.Search[movie].Year}</p>
-            </div>
-
-            <div id="genre-runtime-div">
+                    
                 
-                <p id="genre">${data.Search[movie].Genre}</p>
-                <button id="watchlist-btn">add to watchlist</button>
-                <div id="watch-movie"><a href="https://vidsrc.me/embed/${data.Search[movie].imdbID}/" target="_"><p>Watch Movie</p></a></div>
-            </div>
-
-            <div id="description-div">
-                <p id="movie-description">${data.Search[movie].Plot}</p>
-            </div>
-        </div>
-
-        <img src="${data.Search[movie].Poster}" id="movie-img">
-        `
-            }   
-// insert other fetch here that grabs other info that fetch can't 
-        
-        })
-        
+                })
+                console.log(response)
+                } catch (error) {
+                console.log("Error", error)
+                }
+            } 
+            fetchData()  
+            
     })
 
 
@@ -53,3 +62,51 @@ const watchLink = document.getElementById("watch-movie")
 
 
 
+    // fetch(`https://www.omdbapi.com/?s=${input}&apikey=88e64e6b&s`)
+    //             .then(result => result.json())
+    //             .then(data => {
+                    
+    //                 // movie block HTML
+    //                 movieArray.push(data)
+    
+    //                 for(let movie = 0; movie < 5; movie ++){
+                        
+    //                 document.getElementById("movie-block").innerHTML += `
+    //                 <div id="text-block">
+    //                 <div id="title-rating-div">
+    //                     <h2 id="title">${data.Search[movie].Title}</h2>
+    //                     <p id="year">${data.Search[movie].Year}</p>
+    //                 </div>
+    
+    //                 <div id="genre-runtime-div">
+                        
+    //                     <p id="genre">${data.Search[movie].Genre}</p>
+    //                     <button id="watchlist-btn">add to watchlist</button>
+    //                     <div id="watch-movie"><a href="https://vidsrc.me/embed/${data.Search[movie].imdbID}/" target="_"><p>Watch Movie</p></a></div>
+    //                 </div>
+    
+    //                 <div id="description-div">
+    //                     <p id="movie-description">${movieArray[0]}</p>
+    //                 </div>
+                   
+    //             </div>
+    
+    //             <img src="${data.Search[movie].Poster}" id="movie-img">
+    //             `
+    
+    //             fetch(`https://www.omdbapi.com/?t=${data.Search[movie].Title}&apikey=88e64e6b&s`)
+    //                 .then(result => result.json())
+    //                 .then(watch => {
+    //                     movieArray.push(watch.Title)
+    //                     for (let num of movieArray){
+    //                         console.log(num)
+    //                     }
+    //                 })
+    //             }   
+    
+    
+    // // insert other fetch here that grabs other info that fetch can't 
+            
+    
+                
+    //         })
