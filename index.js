1c31e24a220e39ba3f05396e7ad4215baf8765fb
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2022-12-20 19:48:52 -0700

    reverted function to original
    Reverted back to original function, changed fetch to include all results

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const movieSearch = document.getElementById("movie-search-btn")
+const tvSearch = document.getElementById("tv-search-btn")
 const title = document.getElementById("title")
 const rating = document.getElementById("rating")
 const year = document.getElementById("year")
@@ -8,37 +9,38 @@ const movieImg = document.getElementById("movie-img")
 const watchLink = document.getElementById("watch-movie")
 let html = document.getElementById("movie-block")
 let movieArray = []
-
+let tvArray = []
 
 // movie search functions
 
-    function getMovie() {
+    
        
         movieSearch.addEventListener("click", function(){
             html.innerHTML = ""
-            let input = document.getElementById("movie-search-input").value
-            fetch(`https://www.omdbapi.com/?s=${input}&apikey=88e64e6b&s`)
+            let movieInput = document.getElementById("movie-search-input").value
+            fetch(`https://www.omdbapi.com/?s=${movieInput}&apikey=88e64e6b&s`)
                 .then(res => res.json())
                 .then(data => {
                     for(let movie = 0; movie < 10; movie ++){
-                    // console.log(data.Search[movie].Title)
+                    
                     movieArray.push(data.Search[movie].Title)
-                        // console.log(input)
+                        console.log(data)
                         // console.log(movieArray)
                     }
-                    getData()
+                    getMovieData()
                     
-                    
-                })
-                
-        })
+                })    
+             })
         
-    }
 
-    async function getData(){
+
+    async function getMovieData(){
         for(let movie = 0; movie < 10; movie ++){
-            const response = await fetch(`https://www.omdbapi.com/?apikey=88e64e6b&s&t=${movieArray[movie]}&type=movie`)
+            const response = await fetch(`https://www.omdbapi.com/?apikey=88e64e6b&s&t=${movieArray[movie]}`)
             const search = await response.json()
+            if(search.Type === "series"){
+                let link = ""
+            }
 
             html.innerHTML += `
             <div id="movie-container">
@@ -60,7 +62,15 @@ let movieArray = []
 
                     <div id="description-div">
                         <p id="movie-description">${search.Plot}</p>
-                        <div id="watch-movie"><a href="https://vidsrc.me/embed/${search.imdbID}/" target="_"><p>Watch Movie</p></a></div>
+                        <div id="watch-movie"><a href="https://vidsrc.me/embed/${search.imdbID}/" target="_">
+                        <p>Watch Movie</p></a>
+                        <form id="season-input">
+                            <label for="quantity">Enter Season and Episode #, e.g. Season 1 Episode 2: Enter 12 </label>
+                            <input type="number" id="quantity" name="quantity" min="1" max="5">
+                            <input type="submit" value="Submit">
+                        </form>
+                        </div>
+                        
                     </div>
                 </div>
                 <img src="${search.Poster}" id="movie-img">
@@ -75,6 +85,7 @@ let movieArray = []
             // console.log(search.Genre)
             // console.log(search.imdbID)
             // console.log(search.Poster)
+            console.log(search.Type)
             
         }
         movieArray = []
@@ -82,7 +93,13 @@ let movieArray = []
     }
 
    
-    getMovie()
+       
+    
+
+
+ 
+    
+   
 
 
 
