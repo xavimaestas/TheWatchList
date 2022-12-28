8e66f0a3f1e897f1471f361c1c327d4f8d5b90fa
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2022-12-27 21:56:50 -0700

    working on watchlist buttons
    pushing button changes to watchlist branch

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const movieSearch = document.getElementById("movie-search-btn")
 const tvSearch = document.getElementById("tv-search-btn")
-const title = document.getElementById("title")
+let title = document.getElementById("title")
 const rating = document.getElementById("rating")
 const year = document.getElementById("year")
 const genre = document.getElementById("genre")
 const description = document.getElementById("movie-description")
 const movieImg = document.getElementById("movie-img")
-const watchLink = document.getElementById("watch-movie")
+let watchLink = document.getElementById("watch-movie")
 let html = document.getElementById("movie-block")
+let watchlistHtml = document.getElementById("watchlist-block")
+let tvLink = document.getElementById("tv-link")
 let movieArray = []
+let watchlistArray = []
+let watchlistDisplay = []
 let tvArray = []
-
+let season
+let episode
+let clicked
 
 
 
@@ -29,8 +35,9 @@ let tvArray = []
                     for(let movie = 0; movie < 5; movie ++){
                     
                     movieArray.push(data.Search[movie].Title)
-                        // console.log(data)
-                        console.log(movieArray)
+                    watchlistArray.push(data.Search[movie])
+                        // console.log(watchlistArray)
+                        // console.log(movieArray)
                     }
                     getMovieData()
                     
@@ -43,6 +50,10 @@ let tvArray = []
         for(let movie = 0; movie < 5; movie ++){
             const response = await fetch(`https://www.omdbapi.com/?apikey=88e64e6b&s&t=${movieArray[movie]}`)
             const search = await response.json()
+            let tvEpisode = `${season}-${episode}`
+            let watchlistID = "button"+movie
+            
+            
 
             html.innerHTML += `
             <div id="movie-container">
@@ -58,22 +69,30 @@ let tvArray = []
                     <div id="genre-link-div">
                         <p id="runtime">${search.Runtime}</p>
                         <p id="genre">${search.Genre}</p>
-                        <button id="watchlist-btn">add to watchlist</button>
+                        <button class="watchlist-btn" id="${watchlistID}" type="button">add to watchlist</button>
                         
                     </div>
 
                     <div id="description-div">
                         <p id="movie-description">${search.Plot}</p>
-                        <div id="watch-movie"><a href="https://vidsrc.me/embed/${search.imdbID}/" target="_">
-                        <p>Watch Movie</p></a>
-                      
-                            <label for="season">Enter Season #</label>
-                            <input type="number" id="season" name="season" min="1" max="15">
-                            <label for="episode">Enter Episode # </label>
-                            <input type="number" id="episode" name="season" min="1" max="15">
-                            <button id="season-submit">submit</button>
-                       
-                        </div>
+
+                        <div id="link-list">
+
+                            <div id="watch-movie"><a href="https://vidsrc.me/embed/${search.imdbID}" target="_">
+                            <p>Watch Movie</p></a>
+                            </div>
+
+                            <div id="tv-link"><a href="https://vidsrc.me/embed/${search.imdbID}/${tvEpisode}" target="_">
+                            <p>Watch Movie</p></a>
+                            </div>
+
+                        </div> 
+
+                        <label for="season">Enter Season #</label>
+                        <input type="number" id="season" name="season" min="1" max="15">
+                        <label for="episode">Enter Episode # </label>
+                        <input type="number" id="episode" name="season" min="1" max="15">
+                        <button id="season-submit">submit</button>
                         
                     </div>
                 </div>
@@ -81,27 +100,28 @@ let tvArray = []
             </div>
             <hr> 
             `
-
-            // movieGenreArray.push(movieRatingArray[movie].Genre)
-            // console.log(search)
-            // console.log(movieGenreArray)
-            // console.log(search.Genre)
-            // console.log(search.imdbID)
-            // console.log(search.Poster)
-            document.getElementById("season-submit").addEventListener("click", function(){
-                console.log("submit")
-            })
-        
-            document.getElementById("watchlist-btn").addEventListener("click", function(){
-            console.log("watchlist")  
-                })
+           document.getElementById("button"+movie).addEventListener("click", function(){
+            console.log("click")
+           })
+           
             
+        
+          
         }
         movieArray = []
       
-        console.log(movieArray)
+
+       
+        
+        
+
+
         
     }
+
+
+
+    
 
    
    
